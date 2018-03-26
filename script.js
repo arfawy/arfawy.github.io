1645cfb1405123f484bf3f4b64a700bf0fce30ec
SIDI Benarfa Mze <arfawy1@gmail.com>
2018-03-26 15:12:09 +0100

    Screenshot

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function capture(id) {
     var img;
     html2canvas(document.querySelector(id))
     .then(canvas => {
-        img = canvas.toDataURL();
+        img = canvas.toDataURL("image/png");
         document.getElementById("download").setAttribute("download", "SIDI_Benarfa_Mze.png");
         document.getElementById("download").setAttribute("href", img);
     });
