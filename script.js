f6b33820141db83d76c848a5e95cb2a22be79f0a
SIDI Benarfa Mze <arfawy1@gmail.com>
2018-03-26 21:35:40 +0100

    Logo et amelioration du capture d'ecran

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function capture(id) {
     id = "#" + id;
     var img;
+    var filename = "SIDI_Benarfa_Mze.png";
     html2canvas(document.querySelector(id))
     .then(canvas => {
         img = canvas.toDataURL("image/png");
-        document.getElementById("download").setAttribute("download", "SIDI_Benarfa_Mze.png");
-        document.getElementById("download").setAttribute("href", img);
+
+        var link = document.createElement("a");
+        link.download = filename;
+        link.href = img;
+        document.body.appendChild(link);
+        link.click();
+
+        document.body.removeChild(link);
+        delete link;
+
     });
 }
